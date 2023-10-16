6a68eeb9802d9c0eb6b2bf6d6dee133d8b08ce5c
Guittet <og67guittet@gmail.com>
2023-10-16 15:34:35 +0200

    SAE301

--- GRJOJ/GRJOJ/settings.py
+++ GRJOJ/GRJOJ/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'GRJOJ.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sae301',
+        'USER': 'root',
+        'PASSWORD': '04/11/17',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
